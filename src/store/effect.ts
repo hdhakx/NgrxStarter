2de36dfe59904eb1e5441b0e2b9d040061c37015
hdhakx <himanshudhaka097@gmail.com>
2025-01-19 15:44:32 +0530

    enhancements

--- src/store/effect.ts
+++ src/store/effect.ts
@@ -1,11 +1,10 @@
 import { Injectable } from '@angular/core';
 import { Actions } from '@ngrx/effects';
 import { Store } from '@ngrx/store';
-import { IAppState } from './user.state';
 @Injectable()
 export class UserListEffects {
     constructor(
         private readonly actions$: Actions,
-        private readonly store$: Store<IAppState>
+        private readonly store$: Store<any>
     ) { }
 }
